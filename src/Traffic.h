e254af99bc853b59363f982e91afa62d523484be
johannesfrimberger <johannes.frimberger@gmail.com>
2017-08-12 08:14:26 +0200

    Add basic data elements for traffic

--- src/Traffic.h
+++ src/Traffic.h
@@ -1,14 +1,26 @@
 #ifndef _TRAFFIC_H_
 #define _TRAFFIC_H_
 
+#include "json.hpp"
+
+#include "Coordinates.h"
+
+// for convenience
+using json = nlohmann::json;
+
 /**  */
 class Traffic
 {
 public:
     
-    Traffic();
+    Traffic(const json input);
     
 private:
+    
+    const WorldCoordinates world;
+    const WorldCoordinates speed;
+    const FrenetCoordinates frenet;
+    const unsigned id;
     
 };
 
